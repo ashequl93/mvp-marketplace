4a4b0c87d61ea97b65942c01125c1daa7038a8c4
Ashequl Haque <ashequlhaque009@gmail.com>
2021-12-07 11:21:14 -0500

    initial repo setup

---
+++ client/src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <h2>This is a new MVP marketplace app</h2>
+    )
+  }
+}
+
+export default App;
